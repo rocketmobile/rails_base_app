770c64b16ae67e938e901976994ae412501b3feb
Evan Prothro <evan.prothro@gmail.com>
2013-10-08 14:06:50 -0500

    rails coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'rspec/autorun'
 require 'coveralls'
 
 # Start tracking code coverage metrics
-Coveralls.wear!
+Coveralls.wear! 'rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
