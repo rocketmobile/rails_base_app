2c6ffc0137a0bc0080e682a92ee17b684654a9f6
Evan Prothro <evan.prothro@gmail.com>
2013-10-08 14:12:28 -0500

    _to_back helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def redirect_to_back(opts={})
+    response_status_and_flash = opts.except(:or)
+
     begin
-      redirect_to :back, opts
+      redirect_to :back, response_status_and_flash
     rescue ActionController::RedirectBackError
-      redirect_to(opts[:or] || root_path, opts)
+      redirect_to(opts[:or] || root_path, response_status_and_flash)
     end
+  end
+
+  def link_to_back(text, opts = {})
+    html_options = opts.except(:or)
+    url = request.referrer || opts.delete(:or) || '/'
+
+    link_to text, url, html_options
   end
 end
