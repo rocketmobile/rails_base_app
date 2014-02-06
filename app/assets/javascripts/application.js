3a473f3fec385a65fbf7e9a1377342244f6c8219
James Hafner <james@jameshafner.com>
2014-02-06 11:34:00 -0600

    Added explicit loading of Foundation SASS files and JS files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,12 @@
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
+// Documentation on Foundation javascripts can be found here:
+// http://foundation.zurb.com/docs/javascript.html
+//
+// Many Foundation javascript elements can be controlled via the
+// data-option attribute on an element.
+//
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
@@ -13,8 +19,28 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require foundation
+//= require foundation/foundation
+
 
 $(document).foundation();
 
 $(function(){ $(document).foundation(); });
+
+
+// Foundation scripts that can be imported
+
+// foundation/foundation.abide
+// foundation/foundation.accordion
+// foundation/foundation.alert
+// foundation/foundation.clearing
+// foundation/foundation.dropdown
+// foundation/foundation.equalizer
+// foundation/foundation.interchange
+// foundation/foundation.joyride
+// foundation/foundation.magellan
+// foundation/foundation.offcanvas
+// foundation/foundation.orbit
+// foundation/foundation.reveal
+// foundation/foundation.tab
+// foundation/foundation.tooltip
+// foundation/foundation.topbar
