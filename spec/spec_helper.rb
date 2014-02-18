2f964cd0242fdb2c8a9ae5f68b8c338e31ac011e
Evan Prothro <evan.prothro@gmail.com>
2014-02-18 13:43:42 -0600

    spec helper optimizations for usability

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,11 +10,18 @@ Spork.prefork do
   require 'capybara/rspec'
   require 'factory_girl'
 
-  # # enable use of helpful `screenshot_and_open_image` helper (for integration testing)
-  # # by default the rspec driver will be rack_test, which doesn't support rendering
-  # # if you need a screenshot, mark the spec as javascript (with `, js: true` on the describe call)
-  # # to temporarily use the capybara-webkit driver which supports rendering
+  # enable use of helpful `screenshot_and_open_image` helper (for integration testing)
+  #
+  # note: by default the rspec driver will be rack_test, which doesn't support rendering
+  #       if you need a screenshot, mark the spec as javascript (with `, js: true` on the example call)
+  #       to use the capybara-webkit driver which supports rendering
+  # important: enable `ActiveRecord::Base.shared_connection` logic below if using capybara-webkit
+  #
   # require 'capybara-screenshot/rspec'
+  # Capybara.javascript_driver = :webkit
+  Capybara.javascript_driver = :slenium
+  Capybara.default_wait_time = 10
+  Capybara.server_port = 3121
 
   if !ENV['DRB']
     # Spork not running, calculate test coverage
@@ -42,20 +49,10 @@ Spork.prefork do
     c.include Warden::Test::Helpers
     Warden.test_mode!
 
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    c.use_transactional_fixtures = true
-
     # If true, the base class of anonymous controllers will be inferred
     # automatically. This will be the default behavior in future versions of
     # rspec-rails.
     c.infer_base_class_for_anonymous_controllers = false
-
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run.
-    #     --seed 1234
-    c.order = "random"
   end
 
   # Checks for pending migrations before tests are run.
@@ -63,11 +60,6 @@ Spork.prefork do
 
   # Increase log level to speed tests
   Rails.logger.level = 3
-
-  Capybara.javascript_driver = :slenium
-  # Capybara.javascript_driver = :webkit
-  Capybara.default_wait_time = 10
-  Capybara.server_port = 3121
 end
 
 Spork.each_run do
@@ -96,8 +88,8 @@ Spork.each_run do
   puts "Configuring: reloading factories"
   FactoryGirl.reload
 
-  # ensure transactional rollback works with webkit-capybara
-  # enable if using capybara-webkit
+  # ensure transactional rollback works with out of process `webkit-capybara`
+  # note: required if using capybara-webkit with transactional fixtures
   # ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 
   RSpec.configure do |c|
@@ -110,6 +102,10 @@ Spork.each_run do
     # c.order = "defined"
     # c.order = "rand:60468"
     c.order = "random:#{rand(100000)}"
+
+    # If you're not using ActiveRecord, or you'd prefer not to run each of your
+    # examples within a transaction, remove the following line or assign false
+    c.use_transactional_fixtures = true
   end
 
 end
