9fe83857690836846826fb59c726b97d39574a04
Evan Prothro <evan.prothro@gmail.com>
2014-05-22 17:33:52 -0500

    travis fixup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,14 @@ RSpec.configure do |c|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   c.infer_base_class_for_anonymous_controllers = false
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  c.use_transactional_fixtures = true
+
+  # ensure transactional rollback works with out of process `webkit-capybara`
+  # note: required if using capybara-webkit with transactional fixtures
+  # ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 end
 
 # Checks for pending migrations before tests are run.
@@ -53,36 +61,29 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 # Increase log level to speed tests
 Rails.logger.level = 3
+if defined?(Spring)
+  Spring.after_fork do
+    # running in CI env, calculate test coverage
+    unless ['CI'] == 'true'
+      # calculate coverage locally
+      require 'simplecov'
+      SimpleCov.start 'rails'
+    end
 
-Spring.after_fork do
-  # running in CI env, calculate test coverage
-  unless ['CI'] == 'true'
-    # calculate coverage locally
-    require 'simplecov'
-    SimpleCov.start 'rails'
-  end
+    # reload all factory definitions
+    puts "Configuring: reloading factories"
+    FactoryGirl.reload
 
-  # reload all factory definitions
-  puts "Configuring: reloading factories"
-  FactoryGirl.reload
-
-  # ensure transactional rollback works with out of process `webkit-capybara`
-  # note: required if using capybara-webkit with transactional fixtures
-  # ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
-
-  RSpec.configure do |c|
-    # Run specs in random order to surface order dependencies. If you find an
-    # order dependency and want to debug it, you can fix the order by providing
-    # the seed, which is printed after each run (`'Randomized with seed 60468'`).
-    # `defined` is the default, which executes groups and examples in the
-    # order they are defined in the spec files.
-    #
-    # c.order = "defined"
-    # c.order = "rand:60468"
-    c.order = "random:#{rand(100000)}"
-
-    # If you're not using ActiveRecord, or you'd prefer not to run each of your
-    # examples within a transaction, remove the following line or assign false
-    c.use_transactional_fixtures = true
+    RSpec.configure do |c|
+      # Run specs in random order to surface order dependencies. If you find an
+      # order dependency and want to debug it, you can fix the order by providing
+      # the seed, which is printed after each run (`'Randomized with seed 60468'`).
+      # `defined` is the default, which executes groups and examples in the
+      # order they are defined in the spec files.
+      #
+      # c.order = "defined"
+      # c.order = "rand:60468"
+      c.order = "random:#{rand(100000)}"
+    end
   end
 end
