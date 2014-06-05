1f8fe822603e6efc24768bd5e746bcecc4d3d89d
pierce-h <p.wayne@utexas.edu>
2014-06-05 15:36:55 -0500

    extended spec coverage on lapses

--- spec/requests/api/v1/lapses_spec.rb
+++ spec/requests/api/v1/lapses_spec.rb
@@ -6,7 +6,7 @@ describe "API::V1" do
   let!(:lapse)  { create(:lapse) }
 
   describe "GET /lapses" do
-    it "should return an array of all lapses" do
+    it "returns an array of all lapses" do
       get lapses_path
       expect(response.code.to_i).to eq 200
       expect(json_response).to eq([{
@@ -16,88 +16,159 @@ describe "API::V1" do
         }
       }])
     end
+    it "returns a 200 status code" do
+      get lapses_path
+      expect(response.code.to_i).to eq 200
+    end
   end
 
   describe "GET /lapse/:id" do
-    it "should return a lapse by id" do
-      get lapse_path(lapse)
-      expect(response.code.to_i).to eq 200
-      expect(json_response).to eq({
-        "lapse" => {
-          "id"    => lapse.id,
-          "name"  => lapse.name
-        }
-      })
+    context "with a valid id" do
+      it "returns a lapse by id" do
+        get lapse_path(lapse)
+        expect(json_response).to eq({
+          "lapse" => {
+            "id"    => lapse.id,
+            "name"  => lapse.name
+          }
+        })
+      end
+      it "returns a 200 status code" do
+        get lapse_path(lapse)
+        expect(response.code.to_i).to eq 200
+      end
     end
-    it "should return a 404 error if id is not found" do
-      get lapse_path(-1)
-      expect(response.code.to_i).to eq 404
-      expect(json_response).to eq({
-        "error" => "The requested resource could not be found."
-      })
+
+    context "with an invalid id" do
+      it "returns an error" do
+        get lapse_path(-1)
+        expect(json_response).to eq({
+          "error" => "The requested resource could not be found."
+        })
+      end
+      it "returns a 404 status code" do
+        get lapse_path(-1)
+        expect(response.code.to_i).to eq 404
+      end
     end
   end
 
   describe "POST /lapses" do
-    it "should create and return a lapse" do
-      post '/lapses', {
-        name: 'My Lapse'
-      }.to_json, { 'Content-Type' => 'application/json' }
-      lapse = Lapse.last
-      expect(response.code.to_i).to eq 201
-      expect(json_response).to eq({
-        "lapse" => {
-          "id"    => lapse.id,
-          "name"  => lapse.name
-        }
-      })
+    context "with valid parameters" do
+      it "should create a lapse" do
+        expect{
+          post '/lapses', {
+            name: 'My Lapse'
+          }.to_json, { 'Content-Type' => 'application/json' }
+        }.to change{Lapse.count}.to Lapse.count+1
+      end
+      it "returns a lapse" do
+        post '/lapses', {
+          name: 'My Lapse'
+        }.to_json, { 'Content-Type' => 'application/json' }
+        lapse = Lapse.last
+        expect(json_response).to eq({
+          "lapse" => {
+            "id"    => lapse.id,
+            "name"  => lapse.name
+          }
+        })
+      end
+      it "returns a 201 status code" do
+        post '/lapses', {
+          name: 'My Lapse'
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(response.code.to_i).to eq 201
+      end
     end
-    it "should return an error message when invalid" do
-      post '/lapses', {
-        name: ''
-      }.to_json, { 'Content-Type' => 'application/json' }
-      expect(response.code.to_i).to eq 422
-      expect(json_response).to eq({
-        "error" => ["Name can't be blank"]
-      })
+
+    context "with invalid parameters" do
+      it "returns an error" do
+        post '/lapses', {
+          name: ''
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(json_response).to eq({
+          "error" => ["Name can't be blank"]
+        })
+      end
+      it "returns a 422 status code" do
+        post '/lapses', {
+          name: ''
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(response.code.to_i).to eq 422
+      end
     end
   end
 
   describe "PATCH /lapse/:id" do
-    it "should create and return a lapse" do
-      patch "/lapses/#{lapse.id}", {
-        name: 'My Lapse'
-      }.to_json, { 'Content-Type' => 'application/json' }
-      expect(response.code.to_i).to eq 200
-      expect(json_response).to eq({
-        "lapse" => {
-          "id"    => lapse.reload.id,
-          "name"  => lapse.reload.name
-        }
-      })
+    context "with a valid id" do
+      it "updates the lapse asscoiated with the id" do
+        expect{
+          patch "/lapses/#{lapse.id}", {
+            name: 'My Lapse'
+          }.to_json, { 'Content-Type' => 'application/json' }
+        }.to change{lapse.reload.name}.to 'My Lapse'
+      end
+      it "returns a lapse" do
+        patch "/lapses/#{lapse.id}", {
+          name: 'My Lapse'
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(json_response).to eq({
+          "lapse" => {
+            "id"    => lapse.reload.id,
+            "name"  => lapse.reload.name
+          }
+        })
+      end
+      it "returns a 200 status code" do
+        patch "/lapses/#{lapse.id}", {
+          name: 'My Lapse'
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(response.code.to_i).to eq 200
+      end
     end
-    it "should return an error message when invalid" do
-      patch "/lapses/#{lapse.id}", {
-        name: ''
-      }.to_json, { 'Content-Type' => 'application/json' }
-      expect(response.code.to_i).to eq 422
-      expect(json_response).to eq({
-        "error" => ["Name can't be blank"]
-      })
+
+    context "with an invalid id" do
+      it "returns an error" do
+        patch "/lapses/#{lapse.id}", {
+          name: ''
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(json_response).to eq({
+          "error" => ["Name can't be blank"]
+        })
+      end
+      it "returns a 422 status code" do
+        patch "/lapses/#{lapse.id}", {
+          name: ''
+        }.to_json, { 'Content-Type' => 'application/json' }
+        expect(response.code.to_i).to eq 422
+      end
     end
   end
 
   describe "DELETE /lapse/:id" do
-    it "should remove lapse" do
-      delete lapse_path(lapse)
-      expect(response.code.to_i).to eq 200
+    context "with a valid id" do
+      it "should remove lapse" do
+        expect{
+          delete lapse_path(lapse)
+        }.to change{Lapse.count}.to Lapse.count-1
+      end
+      it "returns a 200 status code" do
+        delete lapse_path(lapse)
+        expect(response.code.to_i).to eq 200
+      end
     end
-    it "should return a 404 error if id is not found" do
-      delete lapse_path(-1)
-      expect(response.code.to_i).to eq 404
-      expect(json_response).to eq({
-        "error" => "The requested resource could not be found."
-      })
+    context "with an invalid id" do
+      it "returns an error" do
+        delete lapse_path(-1)
+        expect(json_response).to eq({
+          "error" => "The requested resource could not be found."
+        })
+      end
+      it "returns a 404 status code" do
+        delete lapse_path(-1)
+        expect(response.code.to_i).to eq 404
+      end
     end
   end
 end
