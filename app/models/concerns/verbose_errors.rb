9043acff06a72f562744911ffc08d6cfaf13a5c3
pierce-h <p.wayne@utexas.edu>
2014-06-10 14:23:22 -0500

    tweaked the param and full messages method

--- app/models/concerns/verbose_errors.rb
+++ app/models/concerns/verbose_errors.rb
@@ -2,18 +2,15 @@ module VerboseErrors
   extend ActiveSupport::Concern
 
   def full_messages_and_parameters
+    messages_and_parameters = []
     if self.try(:errors).present?
-      messages_and_parameters = []
-
       self.errors.keys.each do |parameter|
         messages = self.errors.full_messages_for(parameter)
         messages.each do |message|
-          messages_and_parameters << { 'parameter' => parameter.to_s.titleize, 'message' => message }
+          messages_and_parameters << { 'parameter' => parameter.to_s, 'message' => message }
         end
       end
-      return messages_and_parameters
-    else
-      raise 'No errors present!'
     end
+    messages_and_parameters
   end
 end
